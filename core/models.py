ccab141b338691d77498be02634b13e608cc0199
Michel Atieno <mishqamish@gmail.com>
2019-08-05 09:49:25 +0300

    able to displlay news items on homepage

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class NewsItem(models.Model):
     title = models.CharField(max_length=100)
+    news_story = models.CharField(max_length=1000, default="")
 
     def __str__(self):
         return self.title
